942f54de27a221e0dab560d16147db9f2f6f7ffc
Derp88 <62079853+Derp88@users.noreply.github.com>
2022-03-23 20:41:15 -0400

    New Command to rev shooter and pickup

--- src/main/java/frc/robot/commands/Auto/Modes/Auto3Ball.java
+++ src/main/java/frc/robot/commands/Auto/Modes/Auto3Ball.java
@@ -36,10 +36,14 @@ public class Auto3Ball extends SequentialCommandGroup {
     addCommands(
       new DrivePickup(s_DriveSubsystem, s_PickupSubsystem, s_BeltSubsystem, 40, .45), //Drive with pickup down to get ball
       new PickupArmDownBelts(s_PickupSubsystem, s_BeltSubsystem, PickupConstants.kPickupArmSpeedDown, BeltConstants.kFrontBeltSpeed, BeltConstants.kBackBeltSpeed).withTimeout(1),
+      //NEW CODE TO TEST SOMETIME
+      //new PickupRev(s_PickupSubsystem, s_BeltSubsystem, s_ShooterSubsystem, 0.28, 0.28).withTimeout(1),
       new RunShooterBelt(s_ShooterSubsystem, s_BeltSubsystem, 0.28, 0.28, 1580, BeltConstants.kFrontBeltSpeed, BeltConstants.kBackBeltSpeed).withTimeout(3), //Run the shooter
       new AutoTurn3(s_DriveSubsystem, -.6, 72),
       new DrivePickup(s_DriveSubsystem, s_PickupSubsystem, s_BeltSubsystem, 130, .45),
       new PickupArmDownBelts(s_PickupSubsystem, s_BeltSubsystem, PickupConstants.kPickupArmSpeedDown, BeltConstants.kFrontBeltSpeed, BeltConstants.kBackBeltSpeed).withTimeout(1),
+      //NEW CODE TO TEST SOMETIME
+      //new PickupRev(s_PickupSubsystem, s_BeltSubsystem, s_ShooterSubsystem, 0.32, 0.32).withTimeout(1),
       new AutoTurn3(s_DriveSubsystem, .6, 45),
       new RunShooterBelt(s_ShooterSubsystem, s_BeltSubsystem, 0.32, 0.32, 1790, BeltConstants.kFrontBeltSpeed, BeltConstants.kBackBeltSpeed).withTimeout(3) //Run the shooter
     );
